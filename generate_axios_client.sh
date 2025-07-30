5abe9f992b6754907d17dc51f2eabc5208fff8a5
fzurs <fzursch@gmail.com>
2025-07-29 22:32:35 -0300

    implementando el archivo temporal por bash

--- generate_axios_client.sh
+++ generate_axios_client.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-./manage.py spectacular --color --file schema.yml 
-openapi-generator-cli generate -i schema.yml -g typescript-axios -o frontend/lib/api/
-rm schema.yml
+
+# Crear archivo temporal
+TMPFILE=$(mktemp)
+
+# Generar el schema en el archivo temporal
+./manage.py spectacular --color --file "$TMPFILE"
+
+# Usar el archivo temporal con openapi-generator
+pnpm exec openapi-generator-cli generate -i "$TMPFILE" -g typescript-axios -o frontend/lib/api/
+
+# Eliminar el archivo temporal
+rm "$TMPFILE"
