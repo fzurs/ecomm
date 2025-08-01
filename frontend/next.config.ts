34c5954d20454482b6d48ba8810890f4ed0df010
fzurs <fzursch@gmail.com>
2025-08-01 14:33:58 -0300

    fix: update image paths to use relative URLs instead of localhost

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,13 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "8000",
-      }
-    ]
+    // remotePatterns: [
+    //   {
+    //     protocol: "http",
+    //     hostname: "localhost",
+    //     port: "8000",
+    //   }
+    // ]
 
   }
 };
