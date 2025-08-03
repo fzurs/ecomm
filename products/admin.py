87078e07cf83aa7955761317d7fb55a47bb04125
fzurs <fzursch@gmail.com>
2025-08-03 18:29:26 -0300

    remove all

--- products/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Product, Inventory, Category
-
-admin.site.register([Product, Inventory, Category])
--- products/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-from .views import ProductViewSet, InventoryViewSet
-
-router = routers.DefaultRouter()
-router.register(r'products', ProductViewSet)
-router.register(r'inventory', InventoryViewSet)
-
-urlpatterns = router.urls
