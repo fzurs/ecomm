0d792ce0371377ee0486853a940448597b2a09b5
fzurs <fzursch@gmail.com>
2025-08-18 17:18:26 -0300

    refactor: remove CustomerMetricsCards component from CustomersPage

--- frontend/app/admin/customers/page.tsx
+++ frontend/app/admin/customers/page.tsx
@@ -6,7 +6,6 @@ import { SiteHeader } from "@/components/site-header";
 import { customerSchema } from "./_lib/customer-schema";
 import z from "zod";
 import data from "./_lib/customers.json";
-import { CustomerMetricsCards } from "./_components/customer-metrics-cards";
 
 export default function CustomersPage() {
   const customers = z.array(customerSchema).parse(data);
@@ -17,7 +16,6 @@ export default function CustomersPage() {
       <div className="flex flex-1 flex-col">
         <div className="@container/main flex flex-1 flex-col gap-2">
           <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-            <CustomerMetricsCards customers={customers} />
             <React.Suspense>
               <CustomersTable customers={customers} />
             </React.Suspense>
