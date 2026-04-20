d472d49ba2b3c0a9e3d62e91cfee786c32359214
fzurs <fzursch@gmail.com>
2026-04-20 01:35:58 +0000

    refactor: update docker-compose volumes and improve page formatting; add schema generation script

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,9 @@
 import { Button } from "@workspace/ui/components/button";
 
-export default function Page () {
-    return <h1>Hello<Button>Button</Button></h1>
+export default function Page() {
+  return (
+    <h1>
+      Hello<Button>Button</Button>
+    </h1>
+  );
 }
--- packages/api-client/generate.sh
+++ packages/api-client/generate.sh
@@ -1,2 +1,7 @@
-python ../../apps/api/manage.py spectacular --color --file schema.yml
-pnpm exec openapi-zod-client ./schema.yml -o ./src/zodios/index.ts --export-schemas
+# En el pasado usaba openapi-generator
+# pnpm exec openapi-generator-cli generate -i schema.yml -g typescript-axios -o test
+
+SCHEMA_FILE=$(mktemp)
+python /workspaces/apps/api/manage.py spectacular --color --file "$SCHEMA_FILE"
+pnpm exec openapi-zod-client "$SCHEMA_FILE" -o ./src/zodios/index.ts --export-schemas
+rm "$SCHEMA_FILE"
