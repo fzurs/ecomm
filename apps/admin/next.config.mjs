92e4b96ce31e58b70a4eaf1df22568ccf47bcefd
fzurs <fzursch@gmail.com>
2026-05-01 01:41:10 +0000

    migrate to wsl

--- apps/admin/next.config.mjs
+++ apps/admin/next.config.mjs
@@ -1,8 +1,5 @@
-import webpackConfig from "@workspace/next-config/webpack"
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  ...webpackConfig,
   transpilePackages: ["@workspace/ui"],
 }
 
