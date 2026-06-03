c4496956df00c9726ba095bcb513fb9ba205d608
fzurs <fzursch@gmail.com>
2026-06-03 23:25:22 +0000

    feat: enhance documentation for column filter logic and handling of null values

--- apps/admin/src/hooks/use-column-filters.ts
+++ apps/admin/src/hooks/use-column-filters.ts
@@ -4,8 +4,21 @@ import {
   ColumnFiltersState,
   OnChangeFn,
 } from "@tanstack/react-table"
-import { SingleParser, useQueryStates, Values } from "nuqs"
+import { SingleParser, useQueryStates } from "nuqs"
 import * as React from "react"
+
+// Notas:
+// el column filter es un array vacio
+// nuqs devuelve null, o unknown que puede ser cualquier cosa, 
+// objecto, array, number, date, string, boolean, etc
+// los valores por defecto o los null de nuqs tienen que 
+// matchear con el undefined de columnFilters
+// cuando a nuqs se le manda un null se assigna al defaultvalue 
+// si es que tiene uno
+// un undefined se tendria que transformar en null
+// el array me gusta que desaparezca de la url aun cuando 
+// no tiene un defautlvalue como ([])
+// me gustaria tambien poder desacoplar la logica de esto
 
 type HasFilterParser<K> = K extends {
   id: string
