93a1fc09f53302692554441926eba661e44aaff4
fzurs <fzursch@gmail.com>
2026-06-10 00:46:42 +0000

    feat: refactor product listing logic to use searchParamsCache and improve loading state with Suspense

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,14 +5,15 @@ import { apiClient } from "@/lib/api-client"
 import { SidebarInset } from "@workspace/ui/components/sidebar"
 import { cn } from "@workspace/ui/lib/utils"
 import Link from "next/link"
+import { parseAsString, createSearchParamsCache } from "nuqs/server"
+import { Suspense } from "react"
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: Promise<{ search: string }>
-}) {
-  const { search } = await searchParams
-  const products = await apiClient.products_list({ queries: { search } })
+const searchParamsCache = createSearchParamsCache({
+  search: parseAsString.withDefault(""),
+})
+
+export default async function Page(props: PageProps<"/">) {
+  const params = await searchParamsCache.parse(props.searchParams)
 
   return (
     <>
@@ -21,22 +22,31 @@ export default async function Page({
         <AppSidebar />
         <SidebarInset>
           <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
-            {products.results.map((product) => (
-              <Link key={product.id} href={`/product/${product.slug}`} prefetch>
-                <ProductCard
-                  product={product}
-                  className={cn(
-                    product.brand?.slug === "intel"
-                      ? "hover:border-blue-500"
-                      : "hover:border-orange-500",
-                    "transition-colors h-full"
-                  )}
-                />
-              </Link>
-            ))}
+            <Suspense fallback={<div>Loading...</div>}>
+              <ProductList {...params} />
+            </Suspense>
           </div>
         </SidebarInset>
       </div>
     </>
   )
+}
+
+async function ProductList({ search }: { search?: string }) {
+  "use cache"
+  const products = await apiClient.products_list({ queries: { search } })
+
+  return products.results.map((product) => (
+    <Link key={product.id} href={`/product/${product.slug}`} prefetch>
+      <ProductCard
+        product={product}
+        className={cn(
+          product.brand?.slug === "intel"
+            ? "hover:border-blue-500"
+            : "hover:border-orange-500",
+          "h-full transition-colors"
+        )}
+      />
+    </Link>
+  ))
 }
