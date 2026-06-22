8df00a1cd5dd36dfc2875b7de0f360a437ff4b5d
fzurs <fzursch@gmail.com>
2026-06-22 22:53:13 +0000

    refactor: simplify column filtering logic in DataTableViewOptions

--- apps/admin/src/components/data-table/data-table-view-options.tsx
+++ apps/admin/src/components/data-table/data-table-view-options.tsx
@@ -51,11 +51,7 @@ export function DataTableViewOptions<TData>({
             <CommandGroup>
               {table
                 .getAllColumns()
-                .filter(
-                  (column) =>
-                    typeof column.accessorFn !== "undefined" &&
-                    column.getCanHide()
-                )
+                .filter((column) => column.getCanHide())
                 .map((column) => {
                   return (
                     <CommandItem
