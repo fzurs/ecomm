8d7bb52db49dabb3fee10041dc0d1275b1834f9d
fzurs <fzursch@gmail.com>
2026-08-01 17:24:23 +0000

    fix tailwind data-table

--- apps/admin/src/app/(admin)/page.tsx
+++ apps/admin/src/app/(admin)/page.tsx
@@ -1,22 +1,15 @@
 import {
   AppHeader,
   AppHeaderActions,
-  AppHeaderContent,
-  AppHeaderSeparator,
-  AppHeaderSidebarTrigger,
+  AppHeaderNav,
 } from "@/components/app-header"
-import { NavBreadcrumb } from "@/components/nav-breadcrumb"
 import { ModeToggle } from "@workspace/ui/components/mode-toggle"
 
 export default function Page() {
   return (
     <>
       <AppHeader>
-        <AppHeaderContent>
-          <AppHeaderSidebarTrigger />
-          <AppHeaderSeparator />
-          <NavBreadcrumb items={[{ type: "page", label: "Home" }]} />
-        </AppHeaderContent>
+        <AppHeaderNav items={[{ type: "page", label: "Home" }]} />
         <AppHeaderActions>
           <ModeToggle />
         </AppHeaderActions>
