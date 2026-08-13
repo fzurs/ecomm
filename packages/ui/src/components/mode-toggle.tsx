2b2ba279b6fbc4cf09ea7414195c22d7363a2c9c
fzurs <fzursch@gmail.com>
2026-08-13 17:25:41 +0000

    deleting components that are not used

--- packages/ui/src/components/mode-toggle.tsx
+++ packages/ui/src/components/mode-toggle.tsx
@@ -1,16 +1,9 @@
 "use client"
 
-import * as React from "react"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
 import { Button } from "@workspace/ui/components/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@workspace/ui/components/dropdown-menu"
 
 export function ModeToggle() {
   const { setTheme, resolvedTheme } = useTheme()
